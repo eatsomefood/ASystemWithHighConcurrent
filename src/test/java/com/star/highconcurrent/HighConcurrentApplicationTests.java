878acb2c0e0f9b460ffc909170a603f7a1a55adc
这把不摆了 <13992601+try_to_be_better@user.noreply.gitee.com>
2025-10-24 14:38:19 +0800

    初始化项目

---
+++ src/test/java/com/star/highconcurrent/HighConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.highconcurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
